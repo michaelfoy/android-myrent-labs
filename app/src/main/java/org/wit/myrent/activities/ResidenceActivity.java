a1b8e28684c4df0c5a65d54d119ceeac045fc682
Michael Foy <mikkfoy@gmail.com>
2016-09-30 16:07:46 +0100

    lab02-step08 complete

--- app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
@@ -12,6 +12,8 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import org.wit.myrent.R;
+import org.wit.myrent.app.MyRentApp;
+import org.wit.myrent.models.Portfolio;
 import org.wit.myrent.models.Residence;
 
 public class ResidenceActivity extends AppCompatActivity implements TextWatcher, CompoundButton.OnCheckedChangeListener {
@@ -20,6 +22,7 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher,
     private Residence residence;
     private CheckBox rented;
     private Button dateButton;
+    private Portfolio portfolio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,6 +39,24 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher,
 
         // Register a TextWatcher in the EditText geolocation object
         geolocation.addTextChangedListener(this);
+
+        MyRentApp app = (MyRentApp) getApplication();
+        portfolio = app.portfolio;
+
+        Long resId = (Long) getIntent().getExtras().getSerializable("RESIDENCE_ID");
+        residence = portfolio.getResidence(resId);
+
+        if (residence != null)
+        {
+            updateControls(residence);
+        }
+    }
+
+    public void updateControls(Residence residence)
+    {
+        geolocation.setText(residence.geolocation);
+        rented.setChecked(residence.rented);
+        dateButton.setText(residence.getDateString());
     }
 
     /**
